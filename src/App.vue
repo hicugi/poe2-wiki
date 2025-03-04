415d45e0d6c0a71484435ea98267731af0b532cc
Mag <hicugi@gmail.com>
2025-03-04 20:19:35 +0500

    feat: add images & set inital list

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import UiSearchSelect from './components/Ui/SearchSelect.vue';
+import Card from './components/Card.vue';
 
+const list = ref([]);
 const search = ref('');
+
+const filteredList = computed(() => {
+  const items = list.value;
+  const { value } = search;
+
+  if (value === '') return items;
+
+  return items.filter((el) => el.tags.includes(value));
+});
+
+onMounted(() => {
+  fetch('data.json').then((res) => res.json()).then((data) => {
+    list.value = data;
+  });
+});
 </script>
 
 <template>
@@ -10,13 +27,28 @@ const search = ref('');
     <div class="c-app__header">
       <UiSearchSelect label="Map name" />
     </div>
+
+    <div class="c-app__body">
+      <template v-for="(item, index) in filteredList" :key="index">
+        <Card v-bind="item" />
+      </template>
+    </div>
   </div>
 </template>
 
 <style>
 .c-app__header {
-  margin: 0 auto;
+  margin: 0 auto 40px;
   padding: 20px 0;
   max-width: 700px;
+}
+.c-app__body {
+  margin: 0 auto;
+  padding-bottom: 40px;
+  max-width: 1520px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(min-content, 500px));
+  gap: 10px;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { defineProps, computed } from 'vue';
+
+const props = defineProps({ title: String, image: String, images: Array, tags: Array });
+
+const styles = computed(() => ({
+  backgroundImage: `url('${props.image}')`,
+}));
+</script>
+
+<template>
+  <div class="c-card">
+    <div class="c-card__image" :style="styles" />
+    <h3 class="c-card__title">{{ props.title }}</h3>
+  </div>
+</template>
+
+<style>
+.c-card {
+  position: relative;
+}
+
+.c-card__image {
+  background: no-repeat center;
+  background-size: auto 100%;
+}
+.c-card__image::before {
+  padding-top: calc(9% / 16 * 101);
+  display: block;
+  content: '';
+}
+
+.c-card__title {
+  position: absolute;
+  left: 0;
+  top: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0,0,0,0.6);
+  font-size: 28px;
+  opacity: 0;
+}
+
+.c-card:hover .c-card__title {
+  opacity: 1;
+}
+</style>
